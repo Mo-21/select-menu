e3b0e4f9ed93a1d3da2b06ef5417db6497f201bd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-07-04 15:37:14 +0300

    Add arrow svg

--- src/SelectMenu.tsx
+++ src/SelectMenu.tsx
@@ -76,7 +76,23 @@ const SelectInput = ({ props }: Props) => {
         }}
         className={styles.select_icon}
       >
-        Open
+        <div className={styles.arrow}>
+          <svg
+            width="30px"
+            height="20px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="24" height="24" />
+            <path
+              d="M12 6L12 18M12 18L17 13M12 18L7 13"
+              stroke="#000000"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </div>
       </span>
     </>
   );
